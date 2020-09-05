7679b7cec55f443627690e9b8f8c424a75849935
katafuchix <katafuchix@gmail.com>
2020-09-05 19:14:10 +0900

    Initial Commit

---
+++ SlideMenuSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideMenuSample
+//
+//  Created by cano on 2020/09/05.
+//  Copyright © 2020 cano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
